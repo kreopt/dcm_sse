7f5a82f542a63ee119f1152ccaf82cb5776c5961
kreopt_wf34 <kreopt@gmail.com>
2015-03-31 17:16:24 +0600

    sockets as parameters

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <unordered_map>
 #include <sstream>
 #include "json11.hpp"
-#include <specforge/interprocess/streamsocket/receiver.hpp>
-#include <specforge/dcm/socket/dcm_socket_server.hpp>
+#include <dcm/interprocess/streamsocket/receiver.hpp>
+#include <dcm/socket/dcm_socket_server.hpp>
 using namespace std;
 
 std::string http_headers(){
@@ -36,16 +36,24 @@ std::string comment(const std::string &_cmt){
     return ": "+_cmt+"\n\n";
 }
 
-int main() {
+int main(int argc, char** argv) {
 
-    auto sse_server = interproc::streamsocket::make_receiver(interproc::streamsocket_type::tcp, "127.0.0.1:8888");
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " <sse_port> <unix_socket_path>" << std::endl;
+        return 0;
+    }
+    
+    auto sse_server = interproc::streamsocket::make_receiver(interproc::streamsocket_type::tcp, "0.0.0.0:"+std::string(argv[1]));
     sse_server->on_connect = [](std::shared_ptr<interproc::session<interproc::buffer>> _session){
+        // TODO: read session ID
         _session->send(http_headers());
+        _session->send("retry: 1000\n\n");
+        _session->send("event: ready\ndata:\n\n");
     };
 
     sse_server->start();
 
-    auto dcm_server = dcm::streamsocket::make_receiver(interproc::streamsocket_type::unix, "/home/wf34/proxy.sock");
+    auto dcm_server = dcm::streamsocket::make_receiver(interproc::streamsocket_type::unix, argv[2]);
     dcm_server->on_message = [&sse_server](dcm::message &&_message) {
         //std::cout << "caught " << _message.header.at("signal") << " signal with data: "<< _message.body.at("data") << std::endl;
         std::unordered_map<std::string, std::string> signal;
@@ -55,6 +63,7 @@ int main() {
         }
         signal["event"] = _message.header.at("signal");
         signal["data"] = json11::Json(json).dump();
+        std::cout << "retransmitting " << signal["event"] << std::endl;
         sse_server->broadcast(event_string(signal));
     };
     dcm_server->start();
