dd37271aaa4e9778af5e51556f787daf8dae9d2c
kreopt_wf34 <kreopt@gmail.com>
2015-04-22 18:04:18 +0600

    * update to conform latest dcm api

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <unordered_map>
 #include <sstream>
 #include "json11.hpp"
-#include <dcm/interprocess/streamsocket/receiver.hpp>
+#include <dcm/interprocess/listener_factory.hpp>
 #include <dcm/socket/dcm_socket_server.hpp>
 using namespace std;
 
@@ -42,34 +42,34 @@ int main(int argc, char** argv) {
         std::cout << "Usage: " << argv[0] << " <sse_port> <unix_socket_path>" << std::endl;
         return 0;
     }
-    
-    auto sse_server = interproc::streamsocket::make_receiver(interproc::streamsocket_type::tcp, "0.0.0.0:"+std::string(argv[1]));
+
+    auto sse_server = interproc::make_listener(interproc::conn_type::tcp, "0.0.0.0:"+std::string(argv[1]));
     sse_server->on_connect = [](std::shared_ptr<interproc::session<interproc::buffer>> _session){
         // TODO: read session ID
-        _session->send(http_headers());
-        _session->send("retry: 1000\n\n");
-        _session->send("event: ready\ndata:\n\n");
+        _session->send(interproc::to_buffer(http_headers()));
+        _session->send(interproc::to_buffer("retry: 1000\n\n"));
+        _session->send(interproc::to_buffer("event: ready\ndata:\n\n"));
     };
 
     sse_server->start();
 
-    auto dcm_server = dcm::streamsocket::make_receiver(interproc::streamsocket_type::unix, argv[2]);
-    dcm_server->on_message = [&sse_server](const dcm::message &_message) {
+    auto dcm_server = dcm::streamsocket::make_receiver(interproc::conn_type::unix, argv[2]);
+    dcm_server->on_message = [&sse_server](const dcm::signal &_message) {
         //std::cout << "caught " << _message.header.at("signal") << " signal with data: "<< _message.body.at("data") << std::endl;
         std::unordered_map<std::string, std::string> signal;
         json11::Json::object json;
-        for (auto &k: _message.body){
+        for (auto &k: _message.data()){
             json[k.first] = k.second;
         }
-        signal["event"] = _message.header.at("signal");
+        signal["event"] = _message.name();
         signal["data"] = json11::Json(json).dump();
         std::cout << "retransmitting " << signal["event"] << std::endl;
-        sse_server->broadcast(event_string(signal));
+        sse_server->broadcast(interproc::to_buffer(event_string(signal)));
     };
     dcm_server->start();
 
-    dcm_server->join();
-    sse_server->join();
+    dcm_server->wait_until_stopped();
+    sse_server->wait_until_stopped();
 
     return 0;
 }
